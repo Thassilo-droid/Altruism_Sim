568b831202150e30a99191ae14de35cf35aa8d39
Thassilo-droid <Thassilo.horn@gmx.de>
2021-09-21 19:51:52 +0200

    Autosave

--- Source/source/Enemies/Enemy.py
+++ Source/source/Enemies/Enemy.py
@@ -12,4 +12,13 @@ class Enemy():
     superclass of Panther, etc.
     """
 
-    def __init__(self, species, name, age, sex, apetite, rage, size, strength)
+    def __init__(self, species, name, age, sex, apetite, rage, size, strength):
+        self.species = species
+        self.age = age
+        self.sex = sex
+        self.apetite = apetite
+        self.rage = rage
+        self.size = size
+        self.strength = strength
+
+    def attack
