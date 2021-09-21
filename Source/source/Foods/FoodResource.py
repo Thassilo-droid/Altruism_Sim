51f8506304a47016e1b7ec76b3d41bda52c65d7e
Thassilo-droid <Thassilo.horn@gmx.de>
2021-09-21 20:24:16 +0200

    Autosave

--- Source/source/Foods/FoodResource.py
+++ Source/source/Foods/FoodResource.py
@@ -12,4 +12,14 @@ class FoodResource():
 
     """
 
-    def __init__(self, FoodQuantity, Foods, EnemyExistence, Enemy):
+    def __init__(self, FoodQuantity, Foods, EnemyExistence, \
+                Enemy, free_places, places):
+
+                self.FoodQuantity = FoodQuantity
+                self.Foods = Foods
+                self.EnemyExistence = EnemyExistence
+                self.Enemy = Enemy
+                self.free_places = free_places
+                self.places = places
+
+    
