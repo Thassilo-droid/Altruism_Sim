ade390ba4e01d340bb6c00a9d3ac8560a695ae45
Thassilo-droid <Thassilo.horn@gmx.de>
2021-09-21 20:32:24 +0200

    Autosave

--- Source/source/Foods/Apple.py
+++ Source/source/Foods/Apple.py
@@ -3,4 +3,4 @@ from source.Foods.Food import Food
 class Apple(Food):
 
     def __init__(self):
-        super().__init__("Apple", 10, 0, {"": None})
+        super().__init__("Apple", 10, 0, [""])
