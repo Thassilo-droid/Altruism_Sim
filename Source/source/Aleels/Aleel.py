d86ecf648a79eeac080aad2f6a6108783c68021a
Thassilo-droid <thassilo.horn@gmx.de>
2021-09-21 19:37:38 +0200

    Autosave

--- Source/source/Aleels/Aleel.py
+++ Source/source/Aleels/Aleel.py
@@ -13,9 +13,37 @@ class Aleel(ABC):
     """
 
     def __init__(self, identity, strength, fertability, \
-                cowardness, hp, fed_scale, is_fed, is_alive\
-                has_reproduced, has_partner):
+                cowardness, hp, fed_scale, is_fed=False, is_alive=True\
+                has_reproduced=False, has_partner=False):
         #initializing everything
         self.identity = identity
         self.strength = strength
         self.fertability = fertability
+        self.cowardness = cowardness
+        self.hp = hp
+        self.fed_scale = fed_scale
+        self.is_fed = is_fed
+        self.is_alive = is_alive
+        self.has_reproduced = has_reproduced
+        self.has_partner = has_partner
+
+    def eat(self, food):
+        """
+        eats the food and adds to fed scale
+        """
+        #adds nutrients to fed scale
+        self.fed_scale += food.nutrients
+        #setting is_fed
+        self.is_fed = True
+
+    def reproduce(self, other):
+        #nothing yet to be defined
+        pass
+
+    def search_food(self, food_resource):
+        #not defined yet
+        pass
+
+    def find_home(self, homes):
+        #not yet defined
+        pass
